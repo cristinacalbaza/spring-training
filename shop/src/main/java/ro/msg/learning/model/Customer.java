a6497020037fe49ecf3b1bf09621f25e1e9ed8d8
Cristina Calbaza <Cristina.Calbaza@msg.group>
2020-05-11 21:59:32 +0300

    Consumer, Supplier, ProductCategory created

---
+++ shop/src/main/java/ro/msg/learning/model/Customer.java
@@ -0,0 +1,31 @@
+package ro.msg.learning.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer", schema = "shop")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString @EqualsAndHashCode
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private int id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email_address", nullable = false)
+    private String EmailAddress;
+
+}
---
+++ shop/src/main/java/ro/msg/learning/model/Supplier.java
@@ -0,0 +1,19 @@
+package ro.msg.learning.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "supplier", schema = "shop")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString @EqualsAndHashCode
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+}
